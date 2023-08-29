ffc27f4fa2c96e1554f66a535881f4b37bcd98f7
ElrohirGT <elrohirgt@gmail.com>
2023-08-29 09:06:32 -0600

    feat: Added params for card selector

--- app/src/main/java/com/uvg/gt/cardgenerator/CardSelector.kt
+++ app/src/main/java/com/uvg/gt/cardgenerator/CardSelector.kt
@@ -6,10 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-// TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val ARG_CATEGORIA_NAME = "categoria"
 
 /**
  * A simple [Fragment] subclass.
@@ -17,15 +15,12 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class CardSelector : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var categoria: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            categoria = it.getString(ARG_CATEGORIA_NAME)
         }
     }
 
@@ -42,17 +37,15 @@ class CardSelector : Fragment() {
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
          *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
+         * @param categoryName The name of the category of card to select.
          * @return A new instance of fragment CardSelector.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(categoryName: String) =
             CardSelector().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putString(ARG_CATEGORIA_NAME, categoryName)
                 }
             }
     }
